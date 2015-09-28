ac1f55a4d808f997d91036b51cf5168ec37d8d78
Judgy53 <alexandre.gaubert94@orange.fr>
2015-09-28 12:22:39 +0200

    Added InputMgr

---
+++ MarioKart/Groupe_Rdevaux/SRC/Assets/InputMgr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+public class InputMgr : MonoBehaviour {
+
+    private CarController carController; // Controller of player's car.
+    private CarWaypointHandler wpHandler; // Waypoint handler of player's car.
+
+	void Start () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        carController = player.GetComponent<CarController>();
+        wpHandler = player.GetComponent<CarWaypointHandler>();
+	}
+	
+	void FixedUpdate () {
+	    float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float handbrake = Input.GetAxis("Jump");
+
+        carController.Move(h, v, v, handbrake);
+
+        if (Input.GetButtonDown("Fire1"))
+            wpHandler.TeleportToLastWayPoint();
+    }
+}
---
+++ MarioKart/Groupe_Rdevaux/SRC/Assets/Scripts/NeverGonnaLetYouDown.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeverGonnaLetYouDown : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" || collider.gameObject.tag == "Bot")
+            collider.gameObject.SendMessage("TeleportToLastWayPoint", transform);
+    }
+}
