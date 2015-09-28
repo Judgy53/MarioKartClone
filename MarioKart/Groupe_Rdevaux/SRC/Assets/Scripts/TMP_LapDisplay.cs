97df55a4e4ad854b675cf098c62f84e8c2791f05
Judgy53 <alexandre.gaubert94@orange.fr>
2015-09-28 16:54:30 +0200

    Added lap counting.

---
+++ MarioKart/Groupe_Rdevaux/SRC/Assets/Scripts/TMP_LapDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TMP_LapDisplay : MonoBehaviour {
+
+    private Text text = null;
+    private CarWaypointHandler carWpHandler = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        carWpHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<CarWaypointHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Lap: " + ((int)carWpHandler.Laps).ToString() + "/999$%@#99*8^^";
+    }
+}
