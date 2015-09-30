643ef2c59ac1d83e965a0dca07d4ff9def8288dd
Judgy53 <alexandre.gaubert94@orange.fr>
2015-09-30 16:56:58 +0200

    Added Clock to in-game interface. Replaced cars at start.
    Former-commit-id: ea6481f1919c9a813c6321d45315d18744cf4f8f

---
+++ Assets/Resources/Scripts/Clock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+    private float startTime;
+
+    private bool hasBeenStarted = false;
+
+    public float LocalTime { get { return hasBeenStarted ? Time.time - startTime : 0f; } }
+    public float LocalTimeAsMs { get { return LocalTime * 1000f; } }
+
+    public bool HasBeenStarted { get { return hasBeenStarted; } } 
+
+	public void StartClock()
+    {
+        startTime = Time.time;
+        hasBeenStarted = true;
+    }
+}
---
+++ Assets/Resources/Scripts/TMP_ClockDisplay.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TMP_ClockDisplay : MonoBehaviour {
+
+    private Text text = null;
+    private Clock raceClock = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        raceClock = GameObject.FindGameObjectWithTag("RaceClock").GetComponent<Clock>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int time = (int)raceClock.LocalTimeAsMs;
+        
+        int[] digits = new int[7];
+
+        int ite;
+
+        for (ite = 6; ite >= 0; --ite)
+        {
+            if (ite == 2)
+            {
+                digits[ite] = time % 6;
+                time = (time - digits[ite]) / 6;
+            }
+            else
+            {
+                digits[ite] = time % 10;
+                time = (time - digits[ite]) / 10;
+            }
+        }
+
+        text.text = string.Empty;
+
+        for (ite = 0; ite < 2; ++ite)
+            text.text += digits[ite].ToString();
+
+        text.text += ":";
+
+        for (ite = 2; ite < 4; ++ite)
+            text.text += digits[ite].ToString();
+
+        text.text += ":";
+
+        for (ite = 4; ite < 7; ++ite)
+            text.text += digits[ite].ToString();
+    }
+}
