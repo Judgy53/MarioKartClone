b04f5449c21e44c951f61e5129f37fbda343c697
Judgy53 <alexandre.gaubert94@orange.fr>
2015-10-19 14:45:20 +0200

    Added randoms line to menu.
    Former-commit-id: 0ec59c80f345f373de873f650d62ece362d598be

---
+++ Assets/Scripts/RandomKickLine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RandomKickLine : MonoBehaviour {
+
+    private Text text = null;
+    private string[] lines = null;
+
+	// Use this for initialization
+	void Awake () {
+        text = GetComponent<Text>();
+
+        TextAsset txtAsset = Resources.Load("KickLines") as TextAsset;
+        lines = txtAsset.text.Split(new char[]{'#'});
+	}
+
+    void OnEnable()
+    {
+        text.text = lines[Random.Range(0, lines.Length)];
+    }
+}
